dc71bb113491f99e2c4eb6ba315ae647dadfd00e
zao <805758907@qq.com>
2022-01-12 15:26:55 +0800

    从mesh.h中分离出hash_point和hash_edge

--- DM/Mesh.h
+++ DM/Mesh.h
@@ -2,49 +2,9 @@
 #define RESEARCH_Mesh_H
 
 #include "Face.h"
+#include "hash.h"
 #include <unordered_map>
 #include <stack>
-
-template<typename T>
-inline void hash_combine(std::size_t& seed, const T& val)
-{
-	seed ^= std::hash<T>()(val) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
-}
-
-template<typename T>
-inline void hash_val(std::size_t& seed, const T& val)
-{
-	hash_combine(seed, val);
-}
-
-template<typename T, typename... Types>
-inline void hash_val(std::size_t& seed, const T& val, const Types&... args)
-{
-	hash_combine(seed, val);
-	hash_val(seed, args...);
-}
-
-template<typename... Types>
-inline std::size_t hash_val(const Types& ...args)
-{
-	std::size_t seed = 0;
-	hash_val(seed, args...);
-	return seed;
-}
-class hash_point {          //unordered_map（hashMap）的hash函数，上面都是哈希函数的一部分
-public:
-	size_t operator()(const glm::vec3& p) const {
-		return hash_val(p.x, p.y, p.z);
-	}
-};
-
-class hash_edge {
-public:
-    size_t operator()(const std::pair<int, int> &p) const {
-        return hash_val(p.first, p.second);
-    }
-};
-
 
 class Mesh {
 public:
